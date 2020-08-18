0893bd84dcfd095ddb6046b777f3544ed078bbb2
Thabied Majal <thabied16@gmail.com>
2020-08-18 19:38:24 +0200

    first deployment try

--- app.py
+++ app.py
@@ -5,11 +5,6 @@ import numpy as np
 
 model = pickle.load(open('./xgbmodel.pkl', 'rb'))
 
-def predict(model, input_df):
-    predictions_df = predict_model(estimator=model, data=input_df)
-    predictions = predictions_df['Label'][0]
-    return predictions
-
 def run():
 
     from PIL import Image
@@ -23,32 +18,34 @@ def run():
     ("Single Day", "Upload .csv"))
 
     st.sidebar.info('Using a Machine Learning model to predict the kW production of Solar Panels in Antwerp, Belgium')
+    st.sidebar.info('Please refer to the GitHub repo to view the Weather Mapping for the "Weather Condition" input')
     st.sidebar.success('https://tmplayground.com')
     st.sidebar.success('https://github.com/thabied')
-    st.sidebar.image(image_ground)
+    st.sidebar.image(image_playground)
 
-    st.title("Insurance Charges Prediction App")
+    st.title("Solar Power Prediction Application")
 
     if add_selectbox == 'Single Day':
 
-        age = st.number_input('Age', min_value=1, max_value=100, value=25)
-        sex = st.selectbox('Sex', ['male', 'female'])
-        bmi = st.number_input('BMI', min_value=10, max_value=50, value=10)
-        children = st.selectbox('Children', [0,1,2,3,4,5,6,7,8,9,10])
-        if st.checkbox('Smoker'):
-            smoker = 'yes'
-        else:
-            smoker = 'no'
-        region = st.selectbox('Region', ['southwest', 'northwest', 'northeast', 'southeast'])
+        month = st.number_input('Month', min_value=1, max_value=12, value=5)
+        day = st.number_input('Day', min_value=1, max_value=31, value=16)
+        temp = st.number_input('Temperature (celsius)', min_value=-60, max_value=50, value=25)
+        weather = st.number_input('Weather Condition', min_value=1, max_value=130, value=50)
+        wind = st.number_input('Wind (km/h)', min_value=1, max_value=50, value=14)
+        humidity = st.number_input('Humidity (%)', min_value=1, max_value=100, value=60)
+        barometer = st.number_input('Atmospheric Pressure (milibar)', min_value=1, max_value=2000, value=25)
+        visibility = st.number_input('Visibility (km)', min_value=0, max_value=40, value=25)
 
         output=""
 
-        input_dict = {'age' : age, 'sex' : sex, 'bmi' : bmi, 'children' : children, 'smoker' : smoker, 'region' : region}
+        input_dict = {'month' : month, 'day' : day, 'temp' : temp, 'weather' : weather, 'wind' : wind, 'humidity' : humidity, 'barometer' : barometer, 'visibility' : visibility}
         input_df = pd.DataFrame([input_dict])
+        input_df = scaler.fit_transform(input_df)
 
         if st.button("Predict"):
-            output = predict(model=model, input_df=input_df)
-            output = '$' + str(output)
+
+            output = model.predict(input_df)
+            output = str(output)+'kWs'
 
         st.success('The output is {}'.format(output))
 
@@ -58,7 +55,7 @@ def run():
 
         if file_upload is not None:
             data = pd.read_csv(file_upload)
-            predictions = predict_model(estimator=model,data=data)
+            predictions = model.predict(data)
             st.write(predictions)
 
 if __name__ == '__main__':
