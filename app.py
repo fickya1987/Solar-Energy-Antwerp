e7dc079f308bb0f14474cbe2357dae4bfc970473
Thabied Majal <thabied16@gmail.com>
2020-08-19 20:02:41 +0200

    model load

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 from xgboost import XGBRegressor
 
-model = pickle.load(open('./xgbmodel.json', 'rb'))
+model = xgboost.load_model('./xgbmodel.json')
 
 def run():
 
