081d4d571cf16bc033de30b1eb3fc33c41238725
Thabied Majal <thabied16@gmail.com>
2020-08-27 13:51:52 +0200

    import jdk 7

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import pickle
+import jdk
 import pandas as pd
 import numpy as np
 from xgboost import XGBRegressor
 from pypmml import Model
+
+jdk.install('7')
 
 model = Model.load('./xgbmodel.xml')  # load data
 
